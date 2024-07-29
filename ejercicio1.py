f9f6208fc59e63f5615eb13572c3d8d3586e7095
SENA <SENA@IBAAPRCIIFSP134.FormacionSena.RED>
2024-07-29 17:04:38 -0500

    act1

---
+++ ejercicio1.py
@@ -0,0 +1,26 @@
+mensaje="Hola, estoy aprendiendo python"
+nombre= "Sebastian"
+formacion="ADSO"
+edad=18
+estatura=1.74
+peso=59
+
+print(mensaje, "\n Mi nombre es:", nombre, "\n de la formacion:", formacion, "\n tengo:", edad, "\n mido:", estatura, "\n peso:", peso)
+
+print(f"{mensaje} \n Mi nombre es: {nombre} \n soy de la formacion: {formacion} \n Tengo: {edad} años \n mido {estatura} cm \n peso: {peso}kg")
+
+a=int(input("Ingresa el Digito"))
+b=int(input("Ingresa el segundo digito"))
+
+
+print("la suma de",a,"+",b,"es:",a+b,
+    "\n la resta de",a,"-",b,"es:",a-b,
+    "\n la multiplicacion de",a,"x",b,"es:",a*b,
+    "\n division cociente es:",a//b,
+    "\n division residuo es:",a%b,)
+
+print(f"la suma es {a+b} \n la resta es {a-b} \n la multiplicacion es {a*b} \n el cociente de su division es {a//b} \n el residuo de su division es {a%b}")
+
+int(input("Ingresa el Digito"))
+int(input("Ingresa el segundo digito"))
+
